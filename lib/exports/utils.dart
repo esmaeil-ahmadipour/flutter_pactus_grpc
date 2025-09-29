f3d8c08f7405dd01bc039fdd1d7235a827514696
Esmaeil Ahmadipour <46590079+esmaeil-ahmadipour@users.noreply.github.com>
2025-09-30 02:26:46 +0330

    feat: initial project and add all generated `proto` files (#2)

---
+++ lib/exports/utils.dart
@@ -0,0 +1,4 @@
+export '../src/utils.pb.dart';
+export '../src/utils.pbenum.dart';
+export '../src/utils.pbjson.dart';
+export '../src/utils.pbserver.dart';
---
+++ lib/pactus_grpc.dart
@@ -0,0 +1,5 @@
+export 'exports/blockchain.dart';
+export 'exports/network.dart';
+export 'exports/transaction.dart';
+export 'exports/utils.dart';
+export 'exports/wallet.dart';
